de540a4210636295521532e1dce5375bec9cb00a
||thenishant|| <thenishant3@gmail.com>
2023-06-12 21:48:20 +0530

    Refactor Spending and add options

--- src/expenses/components/Spending.js
+++ src/expenses/components/Spending.js
@@ -1,15 +1,18 @@
-import {useState} from "react";
 import UserData from "./UIElements/UserData";
 import LineChart from "./UIElements/LineChart";
 
 const Spending = () => {
-    const [userData] = useState({
+
+    const userData = {
         labels: UserData.map(data => data.year)
         , datasets: [{
             label: "Total Users",
-            data: UserData.map(data => data.count)
+            data: UserData.map(data => data.count),
+            tension: 0.4,
+            pointRadius: 0
         }]
-    })
+    }
+
 
     return (
         <div className={"line-chart"} style={{width: 400}}>
