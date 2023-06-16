29eaa8ba05e987eb5527f26f987284524a484f57
||thenishant|| <thenishant3@gmail.com>
2023-06-16 22:26:07 +0530

    Fixed Spendings

--- src/expenses/components/Spending.js
+++ src/expenses/components/Spending.js
@@ -1,22 +1,32 @@
 import UserData from "./UIElements/UserData";
-import LineChart from "./UIElements/LineChart";
+import {Line} from "react-chartjs-2";
+import {Chart as ChartJS} from 'chart.js/auto'
+
+import "./Spending.css"
 
 const Spending = () => {
-
     const userData = {
         labels: UserData.map(data => data.month)
         , datasets: [{
-            label: "Spending this year",
             data: UserData.map(data => data.count),
             tension: 0.4,
-            pointRadius: 0
+            pointRadius: 0,
+            borderColor:"orange"
         }]
     }
 
+    const options = {
+        plugins: {
+            legend: {
+                display: false,
+            },
+        },
+    };
 
     return (
-        <div className={"line-chart"} style={{width: 400}}>
-            <LineChart chartData={userData}/>
+        <div className={"line-chart"}>
+            <h4>Expenses trend</h4>
+            <Line options={options} data={userData}/>
         </div>
     );
 }
