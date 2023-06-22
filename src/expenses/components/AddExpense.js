d09d5506d46a841e9e33e8e020761c60fff72de6
||thenishant|| <thenishant3@gmail.com>
2023-06-22 11:08:17 +0530

    Added modal and backdrop

--- src/expenses/components/AddExpense.js
+++ src/expenses/components/AddExpense.js
@@ -1,12 +1,32 @@
 import "./AddExpense.css"
-import {Link} from "react-router-dom";
+import React, {useState} from "react";
+import Modal from "./UIElements/Modal";
+import Button from "./UIElements/Button";
 
 const AddExpense = () => {
+    const [addExpense, setAddExpense] = useState(false)
+    const openAddExpenseHandler = () => setAddExpense(true)
+    const closeAddExpenseHandler = () => setAddExpense(false)
+
     return (
-        <div className={"add-expense"}>
-            <img src={require('../resources/icons/211878_plus_icon.png')} alt={"add-expense-icon"}/>
-            <h4><Link to="/about">Add Expense</Link></h4>
-        </div>
+        <React.Fragment>
+            <Modal
+                show={addExpense}
+                onCancel={closeAddExpenseHandler}
+                header={`Add Expense header`}
+                contentClass={"add-expense__modal-content"}
+                footerClass={"add-expense__modal-actions"}
+                footer={<Button onClick={closeAddExpenseHandler}>CLOSE</Button>}>
+                <div className={"expense-container"}>
+                    <h2>Add New Expense</h2>
+                </div>
+            </Modal>
+
+            <div className={"add-expense"}>
+                <img src={require('../resources/icons/211878_plus_icon.png')} alt={"add-expense-icon"}/>
+                <Button inverse onClick={openAddExpenseHandler}>Add Expense</Button>
+            </div>
+        </React.Fragment>
     );
 }
 
