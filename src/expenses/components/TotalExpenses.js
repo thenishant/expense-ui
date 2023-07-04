749c118ca2b6cbd3522151e76f8c7dc0ff9b0817
||thenishant|| <thenishant3@gmail.com>
2023-07-04 16:04:18 +0530

    Api call to get the expense

--- src/expenses/components/TotalExpenses.js
+++ src/expenses/components/TotalExpenses.js
@@ -1,14 +1,34 @@
 import './TotalExpenses.css'
-
-const EXPENSE_AMOUNT = "₹ 54544";
+import {useEffect, useState} from "react";
 
 const TotalExpenses = () => {
+    const [responseJson, setResponseJson] = useState(null);
+
+    const totalExpenseInAMonthHandler = async () => {
+        const month = "Jul";
+
+        try {
+            const url = new URL("http://localhost:5008/api/expense/getTotalExpenseByMonth");
+            url.search = new URLSearchParams({ month }).toString();
+
+            const response = await fetch(url);
+            const data = await response.json();
+            setResponseJson(data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    useEffect(() => {
+        totalExpenseInAMonthHandler();
+    }, []);
+
     return (
-        <div className={"total-expenses"}>
+        <div className="total-expenses">
             <h2>Total Expenses</h2>
-            <h2>{EXPENSE_AMOUNT}</h2>
+            <h2>{responseJson ? `₹ ${(Object.values(responseJson))}` : "Loading..."}</h2>
         </div>
     );
-}
+};
 
 export default TotalExpenses
