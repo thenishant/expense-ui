bc0687e433751394434c7759ac618de497be9380
||thenishant|| <thenishant3@gmail.com>
2023-07-04 16:04:31 +0530

    Api call to get the AvailableBalance

--- src/expenses/components/AvailableBalance.js
+++ src/expenses/components/AvailableBalance.js
@@ -1,11 +1,29 @@
 import "./AvailableBalance.css"
+import {useEffect, useState} from "react";
 
-const balance = "₹ 654322"
 const AvailableBalance = () => {
+    const [balance, setBalance] = useState(null);
+
+    const totalAvailableBalanceHandler = async () => {
+        try {
+            const url = new URL("http://localhost:5008/api/expense/balance");
+
+            const response = await fetch(url);
+            const data = await response.json();
+            setBalance(data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    useEffect(() => {
+        totalAvailableBalanceHandler();
+    }, []);
+
     return (
         <div className={"available-balance"}>
             <h2>Available Balance</h2>
-            <h3>{balance}</h3>
+            <h3>{balance ? `₹ ${(Object.values(balance))}` : "Loading..."}</h3>
         </div>
     )
 }
