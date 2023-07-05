dbdfcec45d03e994d06b171db639fe9bf7943aae
||thenishant|| <thenishant3@gmail.com>
2023-07-05 16:45:13 +0530

    Add total expense, income, and Balance to Header

--- src/expenses/components/TransactionHeader.js
+++ src/expenses/components/TransactionHeader.js
@@ -1,13 +1,14 @@
-import Card from "./UIElements/Card";
 import './TransactionHeader.css'
 import TotalExpenses from "./TotalExpenses";
 import TotalIncome from "./TotalIncome";
+import TotalBalance from "./TotalBalance";
 
 const TransactionHeader = () => {
     return (
-        <div className='row'>
+        <div className="card-container">
             <TotalExpenses/>
             <TotalIncome/>
+            <TotalBalance/>
         </div>
     )
 }
