a797c9aa5633a856bee85d929ad2172f6d2fed9c
||thenishant|| <thenishant3@gmail.com>
2023-07-06 16:14:22 +0530

    Changed the chart in ExpenseCategory

--- src/expenses/components/ExpensesCategory.js
+++ src/expenses/components/ExpensesCategory.js
@@ -1,18 +1,14 @@
-import {Doughnut} from "react-chartjs-2";
-import {Chart as ChartJS} from 'chart.js/auto'
-
 import "./ExpensesCategory.css"
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from 'react';
+import ReactECharts from 'echarts-for-react';
 
 const ExpensesCategory = () => {
-
     const [expenseCategory, setExpenseCategory] = useState([]);
-    const [selectedMonth, setSelectedMonth] = useState("Jul");
-
+    // const [selectedMonth, setSelectedMonth] = useState("");
     const expenseCategoryHandler = async () => {
-
+        const month = "Jul"
         const url = new URL("http://localhost:5008/api/expense/getMonthlyExpenseByCategory");
-        url.search = new URLSearchParams({month: selectedMonth}).toString();
+        url.search = new URLSearchParams({month}).toString();
 
         try {
             const response = await fetch(url);
@@ -25,67 +21,53 @@ const ExpensesCategory = () => {
 
     useEffect(() => {
         expenseCategoryHandler();
-    }, [selectedMonth]);
+    }, []);
 
-    const backgroundColors = generateRandomColors(expenseCategory.length);
 
-    const userData = {
-        labels: expenseCategory.map(data => data.category),
-        datasets: [{
-            data: expenseCategory.map(data => data.amount),
-            cutout: '65%',
-            hoverOffset: 0,
-            backgroundColor: backgroundColors,
-        }]
-    }
+    const transformedData = expenseCategory.map(item => ({
+        name: item.category,
+        value: item.amount
+    }));
 
-    const options = {
-        plugins: {
-            legend: {
-                display: false,
-            },
+    const option = {
+        title: {
+            text: 'Total Expense',
+            subtext: 'Date',
+            left: 30,
+            top: 30,
+            bottom: 20,
         },
-    };
+        toolbox: {
+            feature: {
+                saveAsImage: {}
+            }
+        },
+        tooltip: {
+            trigger: 'item'
+        },
+        legend: {
+            orient: 'vertical',
+            right: 30,
+            top: 50,
+            bottom: 20,
+        },
+        series: [
+            {
+                type: 'pie',
+                radius: '75%',
+                data: transformedData,
 
-    const handleMonthChange = (e) => {
-        setSelectedMonth(e.target.value);
+            }
+        ]
     };
 
     return (
-        <div className="spend-category">
-            <h4>Expenses by category</h4>
-                <select
-                    id="month-select"
-                    title={"month-select"}
-                    value={selectedMonth}
-                    onChange={handleMonthChange}>
-                    <option value="Jan">January</option>
-                    <option value="Feb">February</option>
-                    <option value="Mar">March</option>
-                    <option value="Apr">April</option>
-                    <option value="May">May</option>
-                    <option value="Jun">June</option>
-                    <option value="Jul">July</option>
-                    <option value="Aug">August</option>
-                    <option value="Sept">September</option>
-                    <option value="Oct">October</option>
-                    <option value="Nov">November</option>
-                    <option value="Dec">December</option>
-                </select>
-            <Doughnut data={userData} options={options}/>
+        <div className={'total-expense-chart'}>
+            <ReactECharts
+                option={option}
+            />
         </div>
-    );
+
+    )
 }
-
-const generateRandomColors = (count) => {
-    const colors = [];
-    for (let i = 0; i < count; i++) {
-        const color = `rgba(${Math.floor(Math.random() * 256)}, ${Math.floor(
-            Math.random() * 256
-        )}, ${Math.floor(Math.random() * 256)}, 0.8)`;
-        colors.push(color);
-    }
-    return colors;
-};
-
 export default ExpensesCategory
