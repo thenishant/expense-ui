5dd3c10e7bbfdd7782ae09788f3139adb4381dff
||thenishant|| <thenishant3@gmail.com>
2023-07-06 16:07:58 +0530

    MOved everything out from mainHeader

--- src/expenses/components/UIElements/MainHeader.js
+++ src/expenses/components/UIElements/MainHeader.js
@@ -1,19 +1,10 @@
 import "./MainHeader.css"
-import CurrentDate from "./CurrentDate";
-import AddExpense from "../AddExpense";
-import AddCategory from "../AddCategory";
 import TransactionHeader from "../TransactionHeader";
 
 const MainHeader = () => {
     return <header className={"main-header"}>
         <TransactionHeader/>
-        <div className={"main-header__content"}>
-            <div className={'main-header__action'}>
-                <AddExpense/>
-                <AddCategory/>
-            </div>
-            <CurrentDate/>
-        </div>
+
     </header>
 
 }
